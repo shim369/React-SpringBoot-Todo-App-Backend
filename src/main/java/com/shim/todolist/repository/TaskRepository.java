39b6b1b7c7d28fa555b89df569803556c1d43d36
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-04-20 18:47:16 +0900

    update TaskRepository

--- src/main/java/com/shim/todolist/repository/TaskRepository.java
+++ src/main/java/com/shim/todolist/repository/TaskRepository.java
@@ -1,5 +1,11 @@
 package com.shim.todolist.repository;
 
-public interface TaskRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.shim.todolist.model.Task;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Integer>{
 
 }
